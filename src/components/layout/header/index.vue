03b780a8f7537c4d166e24c9a0a9ee3b111210d2
leaderli <429243408@qq.com>
2022-03-31 16:11:05 +0800

    init

---
+++ src/components/layout/header/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-row style='width: -webkit-fill-available;'>
+        <el-col :span='2'>
+            <jin-collapse />
+        </el-col>
+        <el-col :span='20'>
+            <jin-tabs class='tablist' />
+        </el-col>
+        <el-col :span='2'>
+            <div class='dropdown'>
+                <el-dropdown>
+                    <img src='@/assets/img/img.jpg'>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item>Action 1</el-dropdown-item>
+                            <el-dropdown-item>Action 2</el-dropdown-item>
+                            <el-dropdown-item @click='signOut'>
+                                退出登录
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+<script setup lang='ts'>
+import JinCollapse from '@/components/layout/header/JinCollapse.vue';
+import JinTabs from '@/components/layout/tabs/JinTabs.vue';
+import { useRouter } from 'vue-router';
+import cookies from '@/util/cookie';
+const router = useRouter();
+const signOut = () => {
+    router.push('/logo');
+    cookies.remove('ms_username');
+};
+</script>
+<style scoped lang='scss'>
+    .dropdown{
+        margin-right: 30px;
+        img{
+            width: 42px;
+            height: 42px;
+            border-radius: 21px;
+            margin-top: 2px;
+        }
+      } 
+
+</style>
+
---
+++ src/router/index.ts
@@ -0,0 +1,166 @@
+import { Router,createRouter, createMemoryHistory, RouteRecordRaw } from 'vue-router';
+import { IMenubar } from '@/type/layout';
+import LayOut from '@/components/layout/index.vue';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const modules = import.meta.globEager('./modules/*.ts');
+const demo:Array<IMenubar> = Object.keys(modules).map((path:string) => modules[path].default);
+
+
+// console.log(demo);
+export const allowRouter: Array<IMenubar> = [
+    {
+        name: '',
+        path: '/',
+        meta: {
+            title: '首页',
+            icon: 'HomeFilled'
+        },
+        component: LayOut,
+        redirect: '/home',
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: () => import('@/views/home/index.vue'),
+                meta: {
+                    title: '首页',
+                    icon: 'HomeFilled'
+                }
+            },
+            {
+                path: '/itema',
+                name: 'itema',
+                component: () => import('@/views/home/itema.vue'),
+                meta: {
+                    title: '首页',
+                    icon: 'HomeFilled'
+                }
+            },
+            {
+                path: '/about',
+                name: 'about',
+                component: () => import('@/views/about/index.vue'),
+                meta: {
+                    title: '关于',
+                    icon: 'Setting'
+                }
+            },
+            {
+                path: '/flow',
+                name: 'flow',
+                component: () => import('@/views/flow/index.vue'),
+                meta: {
+                    title: '流程',
+                    icon: 'Connection'
+                }
+            }
+        ]
+    },
+    ...demo,
+    {
+        path: '/404',
+        name: '404',
+        component: LayOut,
+        meta: {
+            title: '404',
+            icon: 'Warning'
+        },
+        children: [
+            {
+                path: '/404',
+                name: '404',
+                component: () => import('@/views/404/index.vue'),
+                meta: {
+                    title: '404',
+                    icon: 'Warning'
+                }
+            }
+        ]
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/login/index.vue'),
+        meta: {
+            title: 'login',
+            icon: ''
+        }
+    },
+    {
+        path: '/jinscene',
+        name: 'scene',
+        component: LayOut,
+        meta: {
+            title: '菜单',
+            icon: 'Setting'
+        },
+        children: [
+            {
+                path: '/jinscene',
+                name: 'scene',
+                component: () => import('@/views/scene/jinscene.vue'),
+                meta: {
+                    title: '菜单',
+                    icon: 'Setting'
+                }
+            },
+            {
+                path: '/clipboard',
+                name: 'clipboard',
+                component: () => import('@/views/scene/clipboard.vue'),
+                meta: {
+                    title: 'clipboard',
+                    icon: 'Setting'
+                }
+            },
+            {
+                path: '/uploading',
+                name: 'upload',
+                component: () => import('@/views/scene/uploading.vue'),
+                meta: {
+                    title: 'uploading',
+                    icon: 'Setting'
+                }
+            }
+
+        ]
+    },
+
+    {
+        path: '/sysmanage',
+        name: 'sysmanage',
+        component: LayOut,
+        meta: {
+            title: 'Demo',
+            icon: 'Operation'
+        },
+        children:[
+            {
+                path: '/usermanage',
+                name: '模糊查询',
+                component: () => import('@/views/sysmanage/usermanage.vue'),
+                meta: {
+                    title: '模糊查询',
+                    icon: 'Avatar'
+                }
+            },{
+                path: '/download',
+                name: '文件下载',
+                component: () => import('@/views/sysmanage/download.vue'),
+                meta: {
+                    title: '文件下载',
+                    icon: 'Download'
+                }
+            }
+        ]
+    }
+];
+
+const router:Router = createRouter({
+    history: createMemoryHistory(),
+    routes: allowRouter as RouteRecordRaw[]
+});
+
+export default router;
